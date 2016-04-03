54ece468501e0aa960711a217bd829f3d813f89f
lasmc <lasmc24@gmail.com>
2016-04-03 10:40:19 -0500

    funcionalidad suma

---
+++ caluladora_sum.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  InsertSort
+//
+//  Created by Luis Alberto Sanchez Moreno on 3/16/16.
+//  Copyright © 2016 Luis Alberto Sanchez Moreno. All rights reserved.
+//
+
+#include <iostream>
+
+
+using namespace std;
+
+int suma(int a, int b){
+    int c= a+b;
+    return c;
+}
+
+
+int main(){
+    int a;
+    int b;
+    cin >> a >> b;
+    cout<< suma(a,b)<<endl;
+    
+}
